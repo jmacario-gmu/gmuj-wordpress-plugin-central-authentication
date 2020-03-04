343b449c019a7abd558b7440d8fdb4224c16183d
Jan Macario <jmacario@GMU.EDU>
2020-03-04 10:03:45 -0500

    Added plugin URI to plugin meta information.

--- gmuj-wordpress-plugin-central-authentication.php
+++ gmuj-wordpress-plugin-central-authentication.php
@@ -2,6 +2,7 @@
 
 /**
  * Plugin Name:       Mason WordPress: Mason Central Authentication
+ * Plugin URI:        https://github.com/jmacario-gmu/gmuj-wordpress-plugin-central-authentication
  * Description:       Mason WordPress plugin which implements central authentication.
  * Version:           0.0.1
  */
